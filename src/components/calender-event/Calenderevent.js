7077b87f116d9c1f265556866ffecf98881c30b6
young <youngscientist.iysa@gmail.com>
2024-11-26 13:46:41 +0700

    calnder -R

--- src/components/calender-event/Calenderevent.js
+++ src/components/calender-event/Calenderevent.js
@@ -3,7 +3,7 @@ const Calenderevent = () => {
     <section className="calender">
       <div className="wrapper mx-auto text-center">
         <h1 className="h1-calender fw-bold mb-5">
-          Calender Event IYSA 2024
+          Calender Event IYSA 2025
         </h1>
         <iframe className="iframe"
           src="https://heyzine.com/flip-book/8449df0927.html"
