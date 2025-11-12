fda3ad228a7c3d52c377a8f1ee2a21dfe8b0e3c8
iysaofficial <youngscientist.iysa@gmail.com>
2025-11-12 17:12:30 +0800

    upload Newsletter batch3 2025 -k

---
+++ src/pages/newsletter/Newsletter2025DetailPage9.js
@@ -0,0 +1,14 @@
+import Newsletter2025Detail9 from '../../components/newsletter/Newsletter2025Detail9';
+
+// Import CSS
+import '../../css/newsletter/newsletterdetail.css';
+
+function Newsletter2025DetailPage9() {
+    return (  
+      <div>
+        <Newsletter2025Detail9></Newsletter2025Detail9>
+      </div>
+    );
+  }
+  
+export default Newsletter2025DetailPage9;
