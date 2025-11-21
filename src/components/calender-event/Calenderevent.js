bfee761389b24500b0749e0b82806bc5026291bc
iysaofficial <youngscientist.iysa@gmail.com>
2025-11-21 10:49:24 +0700

    update calender Event -k

--- src/components/calender-event/Calenderevent.js
+++ src/components/calender-event/Calenderevent.js
@@ -4,22 +4,11 @@ const Calenderevent = () => {
       <div className="row">
         <div className="col-lg-6 wrapper mx-auto text-center">
           <h2 className="h1-calender fw-bold mb-5">
-            Calendar of Events – IYSA Semester 1, 2025
+            Calendar of Events – IYSA 2026
           </h2>
           <iframe
             className="iframe"
-            src="https://heyzine.com/flip-book/a05c3e5f10.html"
-            allowFullScreen
-            title="calender"
-          ></iframe>
-        </div>
-        <div className="col-lg-6 wrapper mx-auto text-center">
-          <h2 className="h1-calender fw-bold mb-5">
-            Calendar of Events – IYSA Semester 2, 2025
-          </h2>
-          <iframe
-            className="iframe"
-            src="https://heyzine.com/flip-book/60417061c7.html"
+            src="https://heyzine.com/flip-book/a495800b25.html"
             allowFullScreen
             title="calender"
           ></iframe>
