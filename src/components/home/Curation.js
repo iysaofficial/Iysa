d7ec7df5861e340c0b9d79f80e0323c8edfa6e3a
iysaofficial <youngscientist.iysa@gmail.com>
2025-12-04 14:07:01 +0700

    update compro landing -k

--- src/components/home/Curation.js
+++ src/components/home/Curation.js
@@ -5,7 +5,7 @@ const Curation = () => {
       <br></br>
       <div className="container">
         <div className="row">
-          <div className="col-md-6 curation-video mb-auto mt-auto">
+          {/* <div className="col-md-6 curation-video mb-auto mt-auto">
             <iframe
               width="100%"
               height="435px"
@@ -15,9 +15,8 @@ const Curation = () => {
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
             ></iframe>
-          </div>
+          </div> */}
 
-          <div className="col-md-6 curation-content mb-auto mt-auto">
             <h1 className="text-tittle">Company Profile</h1>
             <p className="text-justify">
               IYSA or Indonesian Young Scientist Association is an organization
@@ -30,9 +29,7 @@ const Curation = () => {
             <p className="text-justify">
               It was established in November 2018, and was legally confirmed at
               the Ministry of Law and Human Rights in the form of a notarial
-              deed in 2019. IYSA was founded by Mr. Deni Irawan and later joined
-              forces with Ms. Anggraini and Ms. Prita Elriza in its management
-              team. Anggraini and Ms. Prita Elriza in its management team.
+              deed in 2019 IYSA was founded by Mr. Deni Irawan.
             </p>
             <p className="text-justify">
               Besides having a competition program, IYSA also has various
@@ -44,7 +41,8 @@ const Curation = () => {
               Let's join a research community full of creativity and
               achievements with IYSA!
             </p>
-          </div>
+          {/* <div className="col-md-6 curation-content mb-auto mt-auto">
+          </div> */}
         </div>
       </div>
       <br></br>
